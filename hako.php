e2f423d059486bc488d08321aec2a54515727957
HayatoOsada <ichi_8810@yahoo.co.jp>
2015-03-18 21:32:09 +0900

    Create Plugin Base

--- hako.php
+++ hako.php
@@ -10,3 +10,19 @@ Author URI: http://URI_Of_The_Plugin_Author
 License: A "Slug" license name e.g. GPL2
 */
 
+define( 'WPHAKO2_VERSION', '0.0.1' );
+
+define( 'WPHAKO2_REQUIRED_WP_VERSION', '4.0' );
+
+define( 'WPHAKO2_PLUGIN', __FILE__ );
+
+define( 'WPHAKO2_PLUGIN_BASENAME', plugin_basename( WPHAKO2_PLUGIN ) );
+
+define( 'WPHAKO2_PLUGIN_NAME', trim( dirname( WPHAKO2_PLUGIN_BASENAME ), '/' ) );
+
+define( 'WPHAKO2_PLUGIN_DIR', untrailingslashit( dirname( WPHAKO2_PLUGIN ) ) );
+
+// Deprecated, not used in the plugin core. Use wphako02_plugin_url() instead.
+define( 'WPHAKO2_PLUGIN_URL', untrailingslashit( plugins_url( '', WPHAKO2_PLUGIN ) ) );
+
+require_once WPHAKO2_PLUGIN_DIR . '/settings.php';
---
+++ settings.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: osadahayato
+ * Date: 15/03/18
+ * Time: 21:30
+ */
+
